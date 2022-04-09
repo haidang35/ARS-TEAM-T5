d047729d5e1bb36b60636d0f3645d612dc2428f4
gaminghouse227 <gaminghouse227@gmail.com>
2022-04-09 16:45:42 +0700

    Update

--- backend/Models/Location.cs
+++ backend/Models/Location.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,19 +7,5 @@ namespace backend.Models
 {
     public class Location
     {
-        [Key]
-        public int Id { get; set; }
-        [Required]
-        public string City { get; set; }
-        [Required]
-        public string Province { get; set; }
-        [Required]
-        public string CityCode { get; set; }
-        [Required]
-        public string AirPortName { get; set; }
-        [Required]
-        public string Country { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
     }
 }
