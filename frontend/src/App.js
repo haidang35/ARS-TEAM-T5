e9e4dc78e5e337fd23748e164d2c06325832935d
lamson <sonlamkk011@gmail.com>
2022-04-11 14:46:57 +0700

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Modules/Public/Shared/Components/Navbar/Navbar';
+import Header from './Modules/Admin/Shared/Components/Header/Header';
 
 
 function App() {
   return (
    <>
-  <Navbar />
+  {/* <Navbar /> */}
+  <Header />
    </>
   );
 }
