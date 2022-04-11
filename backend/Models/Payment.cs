59b8b9e06cf30451c4f6b4de9511f4ecabd23b16
Pham Duc <gaminghouse227@gmail.com>
2022-04-11 17:47:10 +0700

    update model

--- backend/Models/Payment.cs
+++ backend/Models/Payment.cs
@@ -1,11 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace backend.Models
 {
+    public enum Status
+    {
+        Active = 1,
+        Deactive = 0
+    }
     public class Payment
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Paymentmethod { get; set; }
+        [Required]
+        public int BookingId { get; set; }
+        public virtual Booking Booking { get; set; }
+        [Required]
+        public double Amount { get; set; }
+        [Required]
+        public Status Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdateAt { get; set; }
     }
 }
