b393d014b4137f1bb2d1bcec41127a7fc9de4c1e
unknown <sonlamkk011@gmail.com>
2022-04-14 11:57:05 +0700

    update

--- frontend/src/Modules/Public/Shared/Components/SearchTiketBox/SortByFLlight/SortByFlight.jsx
+++ frontend/src/Modules/Public/Shared/Components/SearchTiketBox/SortByFLlight/SortByFlight.jsx
@@ -6,7 +6,7 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
 import "./SortByFlight.scss"
-
+import AirplaneTicketIcon from '@mui/icons-material/AirplaneTicket';
 
 
 export class SortByFlight extends Component {
@@ -34,12 +34,14 @@ export class SortByFlight extends Component {
                          <FormControl>
                              <div className="side-bar-filter">
                                  <div className="title-bar">
-                                     <div className="sort-flights">
+                                     {/* <div className="sort-flights"> */}
+                                     <AirplaneTicketIcon className="icon" />
                         <FormLabel
                          className="radio-buttons">Sort By Flight</FormLabel>
+                         
                          </div>
                          </div>
-                         </div>
+                         {/* </div> */}
                          <div className="content">
                              <div className="radio-check">
                                  <div className="item-check">
@@ -51,7 +53,7 @@ export class SortByFlight extends Component {
                             <FormControlLabel value="female" control={<Radio />} label="Flight price (From low to high)" />
                             <FormControlLabel value="male" control={<Radio />} label="Flight price (From high to low)" />
                             <FormControlLabel value="other" control={<Radio />} label="Depart hour" />
-                            <FormControlLabel value="female" control={<Radio />} label="Flight time" />
+                            <FormControlLabel value="flight" control={<Radio />} label="Flight time" />
                             
 
                         </RadioGroup>
