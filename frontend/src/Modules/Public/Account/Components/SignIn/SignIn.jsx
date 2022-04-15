4773cffb66eed68eaa050984f4a3d36388dd84a9
lamson <sonlamkk011@gmail.com>
2022-04-15 14:52:12 +0700

    update

--- frontend/src/Modules/Public/Account/Components/SignIn/SignIn.jsx
+++ frontend/src/Modules/Public/Account/Components/SignIn/SignIn.jsx
@@ -10,7 +10,7 @@ export class SignIn extends Component {
     render() {
         return(
             <>
-            dấdsadsadsasasasasasasasasasasas
+            
             </>
         )
     }
