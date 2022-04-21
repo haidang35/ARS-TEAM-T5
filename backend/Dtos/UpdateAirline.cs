61992eb232a4ca118b8873f169d42624449c1340
ADMIN <ADMIN@DESK^Cgit config --global user.email ADMIN@DESK^C>
2022-04-21 23:07:13 +0700

    update

---
+++ backend/Dtos/UpdateAirline.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace backend.Dtos
+{
+    public class UpdateAirline
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Code { get; set; }
+        [Required]
+        public string Country { get; set; }
+        [Required]
+        public string Logo { get; set; }
+    }
+}
