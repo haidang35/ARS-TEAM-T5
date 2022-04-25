1f2e805f68dda9fb1bf0428ba1b57d52e25a934b
Dang Jinner <nguyenhaidangyq@gmail.com>
2022-04-25 12:06:58 +0700

    Update backend

---
+++ backend/Dtos/PaymentDto.cs
@@ -0,0 +1,19 @@
+﻿using backend.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace backend.Dtos
+{
+    public class PaymentDto
+    {
+        [Required]
+        public PaymentMethod PaymentMethod { get; set; }
+        [Required]
+        public int BookingId { get; set; }
+        [Required]
+        public double Amount { get; set; }
+    }
+}
